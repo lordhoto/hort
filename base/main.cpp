d110ed4d0948e794bd32562d1a8fbd3be02ccc06
Johannes Schickel <lordhoto@gmail.com>
2009-12-06 05:48:40 +0100

    Add some defines for special characters.

--- base/main.cpp
+++ base/main.cpp
@@ -14,8 +14,28 @@ int main(int argc, char *argv[]) {
 		for (int j = 0; j < 21; ++j)
 			win2->printChar('.', i, j);
 	win2->printChar('@', 0, 0, kBlackOnWhite);
-	win2->printChar('S', 20, 5, kGreenOnBlack);
-	win2->printChar('!', 10, 15, kMagentaOnBlack);
+	win2->printChar('S', 22, 7, kGreenOnBlack);
+	win2->printChar('!', 20, 5, kMagentaOnBlack);
+	win2->printChar(kDiamond, 1, 0, kBlueOnBlack);
+	win2->printChar(kDiamond, 1, 1, kBlueOnBlack);
+	win2->printChar(kDiamond, 0, 1, kBlueOnBlack);
+
+	win2->printChar(kUpperLeftEdge, 19, 4);
+	win2->printChar(kHorizontalLine, 20, 4);
+	win2->printChar(kUpperRightEdge, 21, 4);
+	win2->printChar(kVerticalLine, 19, 5);
+	win2->printChar(kVerticalLine, 21, 5);
+	win2->printChar(kTeePointRight, 19, 6);
+	win2->printChar(kHorizontalLine, 20, 6);
+	win2->printChar(kCross, 21, 6);
+	win2->printChar(kVerticalLine, 19, 7);
+	win2->printChar(kVerticalLine, 21, 7);
+	win2->printChar(kVerticalLine, 23, 7);
+	win2->printChar(kLowerLeftEdge, 19, 8);
+	win2->printChar(kHorizontalLine, 20, 8);
+	win2->printChar(kTeePointUp, 21, 8);
+	win2->printChar(kHorizontalLine, 22, 8);
+	win2->printChar(kLowerRightEdge, 23, 8);
 
 	scr.add(win1);
 	scr.add(win2);
--- gui/window.h
+++ gui/window.h
@@ -5,6 +5,19 @@
 
 #include <ncurses.h>
 
+#define kUpperLeftEdge ACS_ULCORNER
+#define kUpperRightEdge ACS_URCORNER
+#define kLowerLeftEdge ACS_LLCORNER
+#define kLowerRightEdge ACS_LRCORNER
+#define kCross ACS_PLUS
+#define kTeePointRight ACS_LTEE
+#define kTeePointLeft ACS_RTEE
+#define kTeePointUp ACS_BTEE
+#define kTeePointDown ACS_TTEE
+#define kVerticalLine ACS_VLINE
+#define kHorizontalLine ACS_HLINE
+#define kDiamond ACS_DIAMOND
+
 namespace GUI {
 
 class Window {
